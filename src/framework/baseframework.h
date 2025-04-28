833aaab007151aba246603189431b40a0a6c1ba8
ciyeer <ciyeer@outlook.com>
2025-04-28 23:01:52 +0800

    refactor(ui): 调整多个UI文件的尺寸和布局
    调整了leftbar.ui、helppage.ui、systempage.ui、accountpage.ui、appearancepage.ui、settingsui.ui、recvwidget.ui和aboutpage.ui的尺寸和布局，优化了界面显示效果。同时，更新了aboutpage.ui中的产品名称和版本信息。

--- src/framework/baseframework.h
+++ src/framework/baseframework.h
@@ -18,7 +18,6 @@ protected:
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
     void mouseReleaseEvent(QMouseEvent *event) override;
-    //void mouseDoubleClickEvent(QMouseEvent *event) override;
     void resizeEvent(QResizeEvent *event) override; // Add this line
 
 private:
